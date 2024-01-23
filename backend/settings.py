ff0b6b7c8d02a7344d5cad5f8f8fd5ac7d755771
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-01-23 13:14:38 +0200

    save

--- backend/settings.py
+++ backend/settings.py
@@ -29,6 +29,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+ALLOWED_HOSTS = [os.environ['APP_DOMAIN']]
+
+# FORM SUBMISSION
+# Comment out the following line and place your railway URL, and your production URL in the array.
+# CSRF_TRUSTED_ORIGINS = ["https://" + os.environ['APP_DOMAIN']]
+
+
 # Application definition
 
 INSTALLED_APPS = [
