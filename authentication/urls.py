18bfe5b88f61e48f18f7ab433d4e0a6c3404bfc5
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-02-26 13:07:44 +0200

    added refresh token

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 urlpatterns = [
    path("auth/google/", views.GoogleLoginApi.as_view(), name="login-with-google"),
    path("googleUserList/", views.GoogleUserListApi.as_view(), name="google-user-list"),
    
    path("auth/regular-login/", views.RegularLoginApi.as_view(), name="regular-login"),
    path("uesr-profile/", views.UserProfileApi.as_view(), name="user-profile"),
+   
+   path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
