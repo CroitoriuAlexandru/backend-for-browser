dc97551417b509eee75751d48d1823ae3964a90c
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2024-03-01 19:23:00 +0200

    asdasdasdasd

--- organization/urls.py
+++ organization/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('set_organization/', views.set_organization),
+    path('set_organization/', views.set_organization.as_view()),
     path('generate_departments/', views.generate_company_departments.as_view()),
     path('set_caen_code/', views.set_caen_code.as_view()),
     path('set_nr_employees/', views.set_nr_employees.as_view()),
